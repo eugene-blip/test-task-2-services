f290348717ebb2574d172cbb82bfdd178ccafcaa
eugene-blip <eugene@algorista.ai>
2025-10-01 17:04:12 +0200

    Fix Typescript errors while running service A container

---
+++ shared/types/events.d.ts
@@ -0,0 +1,46 @@
+export declare enum EventType {
+    DATA_FETCHED = "DATA_FETCHED",
+    FILE_UPLOADED = "FILE_UPLOADED",
+    DATA_INSERTED = "DATA_INSERTED",
+    SEARCH_PERFORMED = "SEARCH_PERFORMED",
+    ERROR_OCCURRED = "ERROR_OCCURRED"
+}
+export interface BaseEvent {
+    eventType: EventType;
+    timestamp: number;
+    serviceId: string;
+    metadata?: Record<string, any>;
+}
+export interface DataFetchedEvent extends BaseEvent {
+    eventType: EventType.DATA_FETCHED;
+    recordCount: number;
+    source: string;
+    duration: number;
+}
+export interface FileUploadedEvent extends BaseEvent {
+    eventType: EventType.FILE_UPLOADED;
+    fileName: string;
+    fileSize: number;
+    fileType: string;
+}
+export interface DataInsertedEvent extends BaseEvent {
+    eventType: EventType.DATA_INSERTED;
+    collectionName: string;
+    recordCount: number;
+    duration: number;
+}
+export interface SearchPerformedEvent extends BaseEvent {
+    eventType: EventType.SEARCH_PERFORMED;
+    query: string;
+    resultCount: number;
+    page: number;
+    limit: number;
+    duration: number;
+}
+export interface ErrorEvent extends BaseEvent {
+    eventType: EventType.ERROR_OCCURRED;
+    error: string;
+    stack?: string;
+    context: string;
+}
+export type ServiceEvent = DataFetchedEvent | FileUploadedEvent | DataInsertedEvent | SearchPerformedEvent | ErrorEvent;
---
+++ shared/types/events.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.EventType = void 0;
+var EventType;
+(function (EventType) {
+    EventType["DATA_FETCHED"] = "DATA_FETCHED";
+    EventType["FILE_UPLOADED"] = "FILE_UPLOADED";
+    EventType["DATA_INSERTED"] = "DATA_INSERTED";
+    EventType["SEARCH_PERFORMED"] = "SEARCH_PERFORMED";
+    EventType["ERROR_OCCURRED"] = "ERROR_OCCURRED";
+})(EventType || (exports.EventType = EventType = {}));
+//# sourceMappingURL=events.js.map
